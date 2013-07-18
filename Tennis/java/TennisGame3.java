b5ee3ab96a6100690badebdac2bb7882a20d7f5c
zaboco <zaboco@gmail.com>
2013-07-18 20:45:09 +0300

    extract separate score methods

--- Tennis/java/TennisGame3.java
+++ Tennis/java/TennisGame3.java
@@ -2,6 +2,7 @@
 public class TennisGame3 implements TennisGame {
 
     public static final int MAXIMUM_GAME_POINTS = 4;
+    public static final String[] SCORE_LABELS = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
     private int secondPlayerPoints;
     private int firstPlayerPoints;
     private String firstPlayerName;
@@ -13,24 +14,38 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String firstPlayerScoreLabel;
-        boolean playersHaveEqualPoints = firstPlayerPoints == secondPlayerPoints;
-        if (firstPlayerPoints < MAXIMUM_GAME_POINTS && secondPlayerPoints < MAXIMUM_GAME_POINTS) {
-            String[] scoreLabels = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            firstPlayerScoreLabel = scoreLabels[firstPlayerPoints];
-            String secondPlayerScoreLabel = playersHaveEqualPoints ? "All" : scoreLabels[secondPlayerPoints];
-            return firstPlayerScoreLabel + "-" + secondPlayerScoreLabel;
+        if (arePointsBelowAdvantage(firstPlayerPoints) && arePointsBelowAdvantage(secondPlayerPoints)) {
+            return getIntermediateScore();
         } else {
-            if (playersHaveEqualPoints)
-                return "Deuce";
-            boolean fistPlayerHasMorePoints = firstPlayerPoints > secondPlayerPoints;
-            String leadingPlayerName = fistPlayerHasMorePoints ? firstPlayerName : secondPlayerName;
-            boolean oneOfThePlayersHasAdvantage = (firstPlayerPoints - secondPlayerPoints) * (firstPlayerPoints - secondPlayerPoints) == 1;
-            String gameStatus = oneOfThePlayersHasAdvantage ? "Advantage " : "Win for ";
-            return gameStatus + leadingPlayerName;
+            return getAdvantageScore();
         }
     }
-    
+
+    private String getAdvantageScore() {
+        if (doPlayersHaveEqualPoints())
+            return "Deuce";
+        boolean doFistPlayerHasMorePoints = firstPlayerPoints > secondPlayerPoints;
+        String leadingPlayerName = doFistPlayerHasMorePoints ? firstPlayerName : secondPlayerName;
+        boolean oneOfThePlayersHasAdvantage = (firstPlayerPoints - secondPlayerPoints) * (firstPlayerPoints - secondPlayerPoints) == 1;
+        String gameStatus = oneOfThePlayersHasAdvantage ? "Advantage " : "Win for ";
+        return gameStatus + leadingPlayerName;
+    }
+
+    private boolean doPlayersHaveEqualPoints() {
+        return firstPlayerPoints == secondPlayerPoints;
+    }
+
+    private String getIntermediateScore() {
+        String firstPlayerScoreLabel;
+        firstPlayerScoreLabel = SCORE_LABELS[firstPlayerPoints];
+        String secondPlayerScoreLabel = doPlayersHaveEqualPoints() ? "All" : SCORE_LABELS[secondPlayerPoints];
+        return firstPlayerScoreLabel + "-" + secondPlayerScoreLabel;
+    }
+
+    private boolean arePointsBelowAdvantage(int firstPlayerPoints) {
+        return firstPlayerPoints < MAXIMUM_GAME_POINTS;
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             this.firstPlayerPoints += 1;
