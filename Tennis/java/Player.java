4a658412577c422131883cb31aebcf6f9f9f2f28
zaboco <zaboco@gmail.com>
2013-07-18 23:10:59 +0300

    add Player class, really

---
+++ Tennis/java/Player.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zaboco
+ * Date: 7/18/13
+ * Time: 8:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+
+    private String name;
+    private int points;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
